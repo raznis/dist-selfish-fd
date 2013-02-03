e6329da3aa75c9b21e09bd8a45cc0d0ed515c5e1
Raz Nissim <raznissim@gmail.com>
2013-02-03 21:49:18 +0200

    minor change to scripts

--- src/parse_results.py
+++ src/parse_results.py
@@ -6,7 +6,8 @@ expanded = 0
 generated = 0
 messages = 0
 time = 0.0
-
+plan_length = 0
+init_h = 0
 for file in range(int(sys.argv[1])):  
   f= open (str(file),"r")
   data = f.read()
@@ -17,9 +18,12 @@ for file in range(int(sys.argv[1])):
   expanded = expanded + int(data[data.find("Expanded ")+9 : data.find(" ", data.find("Expanded ")+9)])
   generated = generated + int(data[data.find("Generated ")+10 : data.find(" ", data.find("Generated ")+10)])
   messages = messages + int(data[data.find("Messages received: ")+19 : data.find("\n", data.find("Messages received: ")+19)])
-  time = max(time, float(data[data.find("Total time: ")+12 : data.find("s", data.find("Total time: ")+12)]))
+  time = max(time, float(data[data.find("Search time: ")+13 : data.find("s", data.find("Search time: ")+13)]))
+  init_h = max(init_h, int(data[data.find("Initial state h value: ")+23 : data.find(".", data.find("Initial state h value: ")+23)]))
+  if data.find("Found solution with g=") > -1:
+    plan_length = int(data[data.find("Found solution with g=")+22 : data.find(",", data.find("Found solution with g=")+22)])
   #print expanded, generated, messages, time
   # Close the file
   f.close() 
-print "Expanded Generated messages time"
-print expanded, generated, messages, time
+print "Expanded Generated messages time cost init_h"
+print expanded, generated, messages, time, plan_length, init_h
